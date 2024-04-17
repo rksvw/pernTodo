6e9775327f74778d405c3cfc734c0351e88b22f4
rksvw <ritik.vw98@gmail.com>
2024-04-17 11:09:55 +0530

    Secure the database

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
 const Pool = require("pg").Pool;
 
+// Retrive the values of the database properties
+const dbHost = process.env.DB_HOST
+const dbPort = process.env.DB_PORT
+const dbUsername = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
+
 const pool = new Pool({
-    user: "postgres",
-    password: "shadowgarden1256956ritik09p",
-    host: "localhost",
-    port: 5432,
-    database: "perntodo"
+    user: dbUsername,
+    password: dbPassword,
+    host: dbHost,
+    port: dbPort,
+    database: dbName
 });
 
 module.exports = pool;
