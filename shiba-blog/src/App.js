50f5bb515e964188e61d068bed67197ee93e2d80
rksvw <ritik.vw98@gmail.com>
2024-04-17 11:29:33 +0530

    frontend is modified

--- shiba-blog/src/App.js
+++ shiba-blog/src/App.js
@@ -2,12 +2,10 @@ import "./App.css";
 import React, { Fragment } from "react";
 import InputTodo from "./components/InputTodo";
 import ListTodo from "./components/ListTodo";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Fragment>
-    <Navbar />
       <InputTodo />
       <ListTodo />
     </Fragment>
